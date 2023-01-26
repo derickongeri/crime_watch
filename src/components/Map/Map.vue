5838818d3365116dd786f71e026f7992b3660aab
derickongeri <derickongeri@gmail.com>
2023-01-26 13:37:19 +0300

    modified Map.vue

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -13,7 +13,7 @@ import baselayers from "./Modals/baselayers.js";
 export default defineComponent({
   setup() {
     const map = ref(null),
-      center = ref([-1, 35]),
+      center = ref([0, 37]),
       baseMaps = ref([]);
 
     const setLeafletMap = function () {
@@ -27,26 +27,28 @@ export default defineComponent({
 
       map.value = L.map("mapid", {
         zoomControl: false,
-        layersControl: false,
+        layersControl: true,
         attributionControl: false,
-        //maxBounds: bounds,
-        minZoom: 3,
-        maxZoom: 17,
-      }).setView(center.value, 3);
+        center: center.value,
+        zoom: 6,
+        zoomAnimation:false,fadeAnimation:true,
+        // layers: [darkMap, osm, mapbox, mapboxSatellite]
+        layers: [satellite]
+      })
 
-      L.tileLayer(
-        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}",
-        {
-          attribution:
-            'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      // L.tileLayer(
+      //   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}",
+      //   {
+      //     attribution:
+      //       'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 
-          id: "mapbox/satellite-v9",
-          accessToken:
-            "pk.eyJ1IjoiY2hyaXNiYXJ0IiwiYSI6ImNrZTFtb3Z2bDAweTMyem1zcmthMGY0ejQifQ.3PzoCgSiG-1-sV1qJvO9Og",
-        }
-      ).addTo(map.value);
+      //     id: "mapbox/satellite-v9",
+      //     accessToken:
+      //       "pk.eyJ1IjoiY2hyaXNiYXJ0IiwiYSI6ImNrZTFtb3Z2bDAweTMyem1zcmthMGY0ejQifQ.3PzoCgSiG-1-sV1qJvO9Og",
+      //   }
+      // ).addTo(map.value);
 
-      L.control.layers(baseMaps.value).addTo(map.value);
+      // L.control.layers(baseMaps.value).addTo(map.value);
     };
 
     onMounted(() => {
@@ -62,10 +64,9 @@ export default defineComponent({
 
 <style scoped>
 #mapid {
-  position: absolute;
+  position: relative;
   top: 0;
-  width: 60%;
-  height: 60%;
+  width: 98%;
   bottom: 0;
   box-shadow: inset 0px 0px 56vw 16vw rgba(0, 0, 0, 0.5);
   border-radius: 20px;
--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,6 +1,13 @@
 <template>
-  <q-page style="background-color:black;">
-    <Mappanel/>
+  <q-page class="row page-body">
+    <div class="col-3" style="">selections</div>
+    <div class="col">
+      <div class="row" style="width:100%;height:15%;"></div>
+      <div class="row" style="width:100%;height:60%"><Mappanel/></div>
+      <div class="row" style="width:100%;height:25%;"></div>
+      </div>
+    <div class="col-4" style="">charts</div>
+
   </q-page>
 </template>
 
@@ -17,4 +24,12 @@ export default defineComponent({
   },
 })
 </script>
+
+<style scoped>
+
+.page-body{
+  box-shadow: inset 0px 0px 56vw 16vw rgba(0, 0, 0, 0.5);
+}
+
+</style>
 
